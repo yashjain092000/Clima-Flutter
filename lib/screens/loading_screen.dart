c7d6bbcbfeb82cb97a801001c599254758430bb8
Yash Jain <46073057+yashjain092000@users.noreply.github.com>
2020-06-03 14:12:55 -0500

    in getData() method use get method from http library and hold it's value in a variable response of type Response and print response.body.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -19,11 +19,13 @@ await location.getCurrentLocation();//using await here we will get latitude and 
     print(location.latitude);
     print(location.longitude);
     }
-    void getData(){
-
-    }
+    void getData() async{
+    http.Response response=await http.get('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02');
+  print(response.body);
+  }
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold();
   }
 }
