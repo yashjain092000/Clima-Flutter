73525247bf837064e84172ee3cb0d24e643aadd3
Yash Jain <46073057+yashjain092000@users.noreply.github.com>
2020-06-03 15:15:41 -0500

    import dart:convert pkg and use it's jsonDecoder to get temperature,condition and cityName of the location.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -19,10 +20,21 @@ await location.getCurrentLocation();//using await here we will get latitude and 
     print(location.latitude);
     print(location.longitude);
     }
-    void getData() async{
-    http.Response response=await http.get('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02');
-  print(response.body);
-  }
+    void getData() async {
+      http.Response response = await http.get(
+          'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02');
+      if (response.statusCode == 200) {
+        String data=response.body;
+        var temperature=jsonDecode(data)['main']['temp'];
+        var condition=jsonDecode(data)['weather'][0]['id'];
+        var cityName=jsonDecode(data)['name'];
+        print(temperature);
+
+      }
+      else {
+        print(response.statusCode);
+      }
+    }
   @override
   Widget build(BuildContext context) {
     getData();
