219d97c1c40037cd8ba9405042dc13fccd013fab
Yash Jain <46073057+yashjain092000@users.noreply.github.com>
2020-06-03 15:40:12 -0500

    declare 2 double type variable latitude and longitude and initialize them with the location's latitude and longitude values.Also make the changes in the url and call getData() method in getCurrentLocation() method.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-String apiKey='a6c0198e589f0709c878c789bd81aa9e';
 
+String apiKey = 'a6c0198e589f0709c878c789bd81aa9e';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -11,36 +11,43 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude;
+  double longitude;
+
   @override
-  void initState(){
+  void initState() {
     super.initState();
     getLocation();
   }
-  void getLocation() async{
-Location location=Location();
-await location.getCurrentLocation();//using await here we will get latitude and longitude printed only when it's execution gets completed.
-    print(location.latitude);
-    print(location.longitude);
-    }
-    void getData() async {
-      http.Response response = await http.get(
-          'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02');
-      if (response.statusCode == 200) {
-        String data=response.body;
-        var decodedData=jsonDecode(data);
-        double temperature=decodedData['main']['temp'];
-        int condition=decodedData['weather'][0]['id'];
-        String cityName=decodedData['name'];
-        print(temperature);
 
-      }
-      else {
-        print(response.statusCode);
-      }
+  void getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    //using await here we will get latitude and longitude printed only when it's execution gets completed.
+    latitude = location.latitude;
+    longitude = location.longitude;
+    getData();
+  }
+
+  void getData() async {
+    http.Response response = await http.get(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+    if (response.statusCode == 200) {
+      String data = response.body;
+      var decodedData = jsonDecode(data);
+      double temperature = decodedData['main']['temp'];
+      int condition = decodedData['weather'][0]['id'];
+      String cityName = decodedData['name'];
+      print(temperature);
+      print(condition);
+      print(cityName);
+    } else {
+      print(response.statusCode);
     }
+  }
+
   @override
   Widget build(BuildContext context) {
-    getData();
     return Scaffold();
   }
 }
