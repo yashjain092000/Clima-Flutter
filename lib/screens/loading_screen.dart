7bc0296bcd1cdc7124b650b30fd9f527e31479e7
Yash Jain <46073057+yashjain092000@users.noreply.github.com>
2020-06-03 15:57:19 -0500

    in networking file incorporate the getData() method functionality and in loading_screen make a single method getLocationData() and make the required changes.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:clima/services/networking.dart';
 
 String apiKey = 'a6c0198e589f0709c878c789bd81aa9e';
 
@@ -17,33 +16,20 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   void initState() {
     super.initState();
-    getLocation();
+    getLocationData();
   }
 
-  void getLocation() async {
+  void getLocationData() async {
     Location location = Location();
     await location.getCurrentLocation();
     //using await here we will get latitude and longitude printed only when it's execution gets completed.
     latitude = location.latitude;
     longitude = location.longitude;
-    getData();
-  }
-
-  void getData() async {
-    http.Response response = await http.get(
+    NetworkHelper networkHelper=NetworkHelper(
         'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
-    if (response.statusCode == 200) {
-      String data = response.body;
-      var decodedData = jsonDecode(data);
-      double temperature = decodedData['main']['temp'];
-      int condition = decodedData['weather'][0]['id'];
-      String cityName = decodedData['name'];
-      print(temperature);
-      print(condition);
-      print(cityName);
-    } else {
-      print(response.statusCode);
-    }
+
+      var weatherData= await networkHelper.getData();
+
   }
 
   @override
--- lib/services/networking.dart
+++ lib/services/networking.dart
@@ -3,5 +3,15 @@ import 'dart:convert';
 class NetworkHelper{
 NetworkHelper(this.url);
 final String url;
+Future getData() async{
+  http.Response response = await http.get(url);
+  if (response.statusCode == 200) {
+    String data = response.body;
+    return jsonDecode(data);
+  }
+  else {
+    print(response.statusCode);
+  }
 
+}
 }
