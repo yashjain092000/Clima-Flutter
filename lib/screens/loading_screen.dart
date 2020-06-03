e9fab9078bf4769da2680ad83141beb1ab8a3a04
Yash Jain <46073057+yashjain092000@users.noreply.github.com>
2020-06-03 13:27:37 -0500

    add http pkg in pubspec.yaml file and import it in loading_screen.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:clima/services/location.dart';
+import 'package:http/http.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
