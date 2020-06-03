6b827d2f5ef83135c8400342e73789501cdce23e
Yash Jain <46073057+yashjain092000@users.noreply.github.com>
2020-06-03 15:17:16 -0500

    instead of jsonDecode() declare a variable decodedData and use it instead.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -25,9 +25,10 @@ await location.getCurrentLocation();//using await here we will get latitude and 
           'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02');
       if (response.statusCode == 200) {
         String data=response.body;
-        var temperature=jsonDecode(data)['main']['temp'];
-        var condition=jsonDecode(data)['weather'][0]['id'];
-        var cityName=jsonDecode(data)['name'];
+        var decodedData=jsonDecode(data);
+        var temperature=decodedData['main']['temp'];
+        var condition=decodedData['weather'][0]['id'];
+        var cityName=decodedData['name'];
         print(temperature);
 
       }
